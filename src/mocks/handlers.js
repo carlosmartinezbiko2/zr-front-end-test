2f7703c57f43d10014b037feb4eefb8d649905c8
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-15 11:33:30 +0200

    Test: configures msw para product detail

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,9 +1,14 @@
 import { rest } from 'msw'
 import productList from './fixtures/productList.json'
+import productDetail from './fixtures/productDetail.json'
 
 export const handlers = [
   rest.get(
     'https://front-test-api.herokuapp.com/api/product',
     (req, res, ctx) => res(ctx.status(200), ctx.json(productList)),
+  ),
+  rest.get(
+    'https://front-test-api.herokuapp.com/api/product/ZmGrkLRPXOTpxsU4jjAcv',
+    (req, res, ctx) => res(ctx.status(200), ctx.json(productDetail)),
   ),
 ]
