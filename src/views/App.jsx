7e2f6a20087d5cd0047df558d39ea464a6ce0e22
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-15 08:33:02 +0200

    Refactor: extrae páginas a componentes

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+import { ProductDetailsPage } from './ProductDetailsPage'
+import { ProductListPage } from './ProductListPage'
+
 export function App() {
   return (
     <>
       <header>ZR Mobile</header>
       <Switch>
         <Route exact path="/">
-          Lista de productos
+          <ProductListPage />
         </Route>
-        <Route
-          path="/:productId"
-          render={({ match }) => <>{match.params.productId}</>}
-        />
+        <Route path="/:productId">
+          <ProductDetailsPage />
+        </Route>
       </Switch>
     </>
   )
