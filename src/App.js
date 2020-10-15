54a33444b0142825e699452fc4f412ef3a842e0f
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-15 08:11:53 +0200

    Muestra la página de productos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 export function App() {
   return (
     <>
       <header>ZR Mobile</header>
+      <Switch>
+        <Route exact path="/">
+          Lista de productos
+        </Route>
+      </Switch>
     </>
   )
 }
