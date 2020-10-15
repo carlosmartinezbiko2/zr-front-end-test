517e80614625bdfa6f942334878b12f243c54b61
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-15 10:48:48 +0200

    Test: filtra por el campo de búsqueda

--- src/views/ProductListPage/ProductListPage.test.js
+++ src/views/ProductListPage/ProductListPage.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import productList from 'mocks/fixtures/productList.json'
 
@@ -14,5 +14,17 @@ describe('ProductListPage', () => {
 
       expect(productModelText).toBeInTheDocument()
     }
+  })
+
+  it('Filtra por el campo de búsqueda', async () => {
+    render(<ProductListPage />)
+
+    const searchInput = screen.getByRole('textbox', { name: /buscar:/i })
+
+    fireEvent.change(searchInput, { value: 'Iconia B1' })
+
+    const products = await screen.findAllByRole('article')
+
+    expect(products).toHaveLength(2)
   })
 })
