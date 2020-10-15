eefddf2f83f157dd57275ed2f69a077cf453857a
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-15 08:59:08 +0200

    Muestra listado productos del API

--- src/views/ProductListPage/ProductListPage.jsx
+++ src/views/ProductListPage/ProductListPage.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
 
 export function ProductListPage() {
-  return <>Lista de productos</>
+  const [productList, setProductList] = React.useState()
+  React.useEffect(() => {
+    getProductList().then(setProductList)
+  }, [])
+
+  return (
+    <>
+      Lista de productos
+      {productList?.map((product) => (
+        <article key={product.id}>{product.model}</article>
+      ))}
+    </>
+  )
+}
+
+async function getProductList() {
+  const response = await fetch(
+    'https://front-test-api.herokuapp.com/api/product',
+  )
+  const productList = await response.json()
+  return productList
 }
