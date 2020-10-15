759b171c12c933c2bfb0b2e574d9c7ab4e2bccb4
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-15 11:03:58 +0200

    Navega al listado al hacer click en el header

--- src/views/components/Layout/Header/Header.jsx
+++ src/views/components/Layout/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { HeaderContainer, TitleWrapper } from './Header.styles'
 import { HeaderIcon } from './HeaderIcon'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <TitleWrapper>
-        <HeaderIcon />
-        ZR Mobile
-      </TitleWrapper>
+      <Link to="/">
+        <TitleWrapper>
+          <HeaderIcon />
+          ZR Mobile
+        </TitleWrapper>
+      </Link>
     </HeaderContainer>
   )
 }
