5fd2639ad932bab61464ff16181b165af83702aa
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-16 07:50:28 +0200

    Estilos: agrega fuente

--- src/theme/GlobalStyles/reboot.js
+++ src/theme/GlobalStyles/reboot.js
@@ -40,6 +40,12 @@ export const reboot = css`
     #root {
       height: 100%; /* 7 */
     }
+
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   -ms-viewport {
