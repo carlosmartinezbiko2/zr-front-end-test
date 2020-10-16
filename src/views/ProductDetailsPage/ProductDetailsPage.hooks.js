5019d0ab6ef79b1000bb38a5882ba4f6e5971c63
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-16 09:39:04 +0200

    Agrega caché

--- src/views/ProductDetailsPage/ProductDetailsPage.hooks.js
+++ src/views/ProductDetailsPage/ProductDetailsPage.hooks.js
@@ -5,6 +5,7 @@ export function useProductDetail(id) {
   const { data: productDetail, isLoading, isError } = useQuery(
     ['productList', id],
     () => productService.getProductDetail(id),
+    { staleTime: 60 * 60 * 1000, cacheTime: 60 * 60 * 1000 },
   )
 
   return { productDetail, isLoading, isError }
