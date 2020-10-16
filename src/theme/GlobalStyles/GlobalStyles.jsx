2234b293018ff8f6d181a71c40d6df0973ad5a92
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-16 08:41:06 +0200

    Fix: mostrar scrollbar

--- src/theme/GlobalStyles/GlobalStyles.jsx
+++ src/theme/GlobalStyles/GlobalStyles.jsx
@@ -8,26 +8,6 @@ import { sizes } from '../size'
 
 const scrollStyles = css`
   scroll-behavior: smooth;
-
-  @supports (-webkit-overflow-scrolling: touch) {
-    * {
-      -webkit-overflow-scrolling: touch;
-    }
-  }
-  @supports not (-webkit-overflow-scrolling: touch) {
-    overflow: auto;
-
-    ::-webkit-scrollbar {
-      width: ${rem(sizes.tiny)};
-      border: 0;
-      background-color: transparent;
-    }
-
-    ::-webkit-scrollbar-thumb {
-      background-color: ${colors.darkGray};
-      border-radius: 0;
-    }
-  }
 `
 
 const globalStyles = css`
