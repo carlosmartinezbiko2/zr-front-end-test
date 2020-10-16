d95418b6f1cd7b40cff9f7df42909289c21809ba
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-16 08:02:04 +0200

    Estilos: aplica tema en DescriptionItem

--- src/views/components/DescriptionItem/DescriptionItem.styles.jsx
+++ src/views/components/DescriptionItem/DescriptionItem.styles.jsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 import { rem } from 'polished'
+import { colors, sizes } from 'theme'
 
 export const DescriptionItemWrapper = styled.div`
   display: flex;
   justify-content: space-between;
 
-  padding: ${rem(8)} 0;
+  padding: ${rem(sizes.tiny)} 0;
 
-  border-bottom: 2px solid #eee;
+  border-bottom: 2px solid ${colors.gray};
 `
 
 export const DescriptionItemTitle = styled.span`
