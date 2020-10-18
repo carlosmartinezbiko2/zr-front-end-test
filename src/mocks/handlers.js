15809b20b9354db6eb57cebe51b1d4592fa6d6ac
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-18 12:08:53 +0200

    Format: aplica formato linter

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,9 +3,8 @@ import productList from './fixtures/productList.json'
 import productDetail from './fixtures/productDetail.json'
 
 export const handlers = [
-  rest.get(
-    `${process.env.REACT_APP_API_URL}/product`,
-    (req, res, ctx) => res(ctx.status(200), ctx.json(productList)),
+  rest.get(`${process.env.REACT_APP_API_URL}/product`, (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(productList)),
   ),
   rest.get(
     `${process.env.REACT_APP_API_URL}/product/ZmGrkLRPXOTpxsU4jjAcv`,
