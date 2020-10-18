f18f0d991e127398fdafc6ba56bde2107aa13c29
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-18 12:08:01 +0200

    Fix: arregla  gestión de errores de fetch

--- src/core/cartService.js
+++ src/core/cartService.js
@@ -1,18 +1,20 @@
 async function addProduct(id, colorCode, storageCode) {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/cart`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id,
-        colorCode: +colorCode,
-        storageCode: +storageCode,
-      }),
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/cart`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  )
+    body: JSON.stringify({
+      id,
+      colorCode: +colorCode,
+      storageCode: +storageCode,
+    }),
+  })
+
+  if (!response.ok) {
+    throw new Error('Request error')
+  }
+
   const data = await response.json()
 
   return data
--- src/core/productService.js
+++ src/core/productService.js
@@ -1,15 +1,21 @@
 async function getProductList() {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/product`,
-  )
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/product`)
+
+  if (!response.ok) {
+    throw new Error('Request error')
+  }
+
   const productList = await response.json()
   return productList
 }
 
 async function getProductDetail(id) {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/product/${id}`,
-  )
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/product/${id}`)
+
+  if (!response.ok) {
+    throw new Error('Request error')
+  }
+
   const productList = await response.json()
   return productList
 }
