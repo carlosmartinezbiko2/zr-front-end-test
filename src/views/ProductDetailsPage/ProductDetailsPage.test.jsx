88006b230302508af2dfbceb71864c8c57a3311a
Carlos Martinez <carlos.martinez@biko2.com>
2020-10-18 12:43:48 +0200

    Test: muestra error en página de detalle de producto

--- src/views/ProductDetailsPage/ProductDetailsPage.test.jsx
+++ src/views/ProductDetailsPage/ProductDetailsPage.test.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import { screen } from '@testing-library/react'
 import { Route } from 'react-router-dom'
+import { rest } from 'msw'
 import { renderWithRouter } from 'utils/test/renderWithRouter'
 
+import { server } from 'mocks/server'
 import { ProductDetailsPage } from './ProductDetailsPage'
 
 describe('ProductDetailsPage', () => {
+  it('Mustra un mensaje de error en caso de fallar la petición al API', async () => {
+    const productId = 'ZmGrkLRPXOTpxsU4jjAcv'
+    server.use(
+      rest.get(
+        `${process.env.REACT_APP_API_URL}/product/${productId}`,
+        (req, res, ctx) => res(ctx.status(500), ctx.json({ error: 'Error' })),
+      ),
+    )
+
+    renderWithRouter(
+      <Route path="/:productId">
+        <ProductDetailsPage />
+      </Route>,
+      `/${productId}`,
+    )
+    const errorText = await screen.findByText('Ups! Ha ocurrido un error')
+
+    expect(errorText).toBeInTheDocument()
+  })
+
   it('Muestra el detalle del producto del API', async () => {
     const productId = 'ZmGrkLRPXOTpxsU4jjAcv'
     const productBrand = 'Acer'
